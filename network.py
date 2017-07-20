ac14f89e603adf5d4bfb3f351bff84b7225c2532
Na'Shea Wiesner <nashea.wiesner@centricient.com>
2017-07-19 22:32:17 -0600

    model working

--- network.py
+++ network.py
@@ -1,6 +1,10 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
+import csv
+import numpy as np
+import random
+
 
 '''
 input > weight > hidden layer 1(activation function) > weights > hidden layer 2(activation function) > weights > output layer
@@ -14,34 +18,59 @@ feed forward + backprop = epoch
 
 '''
 
+with open('dataset.csv', 'r') as f:
+    reader = csv.reader(f)
+    counter = 0
+    featureSet = []
+    labelSet = []
+    for row in reader:
+        if counter == 0:
+            random.shuffle(row)
+        features = []
+        labels = []
+        for i in range(68,195):
+            features.append(row[i])
+        features = np.array(features)
+        featureSet.append(features)
+        #for j in range(197,198 ):#227
+        labels.append(row[226])
+        if labels[0] == '0':
+            labels.append('1')
+        elif labels[0] == '1':
+            labels.append('0')
+        labels = np.array(labels)
+        labelSet.append(labels)
+        counter += 1
 
-#from tensorflow.examples.tutorials.mnist import input_data
+testing_size = int(0.1 * len(features))
 
-#mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-
+train_x = list(featureSet[:-testing_size])
+train_y = list(labelSet[:-testing_size])
+test_x = list(featureSet[-testing_size:])
+test_y = list(labelSet[-testing_size:])
 
 n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
+#n_nodes_hl2 = 500
+#n_nodes_hl3 = 500
 
-n_classes = 31
-batch_size = 50
+n_classes = 2
+
 
 #height X width
-x = tf.placeholder('float',[None, 242])
+x = tf.placeholder('float',[None,len(train_x[0])])
 y = tf.placeholder('float')
+
+batch_size = 50
 
 def neural_network_model(data):
 
-
-
-    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([242, n_nodes_hl1])),
+    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([len(train_x[0]), n_nodes_hl1])),
                       'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
-    hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
-    hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
-    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+    #hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                      #'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
+    #hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl3])),
+                      #'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
+    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_classes])),
                       'biases': tf.Variable(tf.random_normal([n_classes]))}
 
 #(input_data * weights) + biases
@@ -49,13 +78,13 @@ def neural_network_model(data):
     l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
     l1 = tf.nn.relu(l1)
 
-    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
-    l2 = tf.nn.relu(l2)
+    #l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
+    #l2 = tf.nn.relu(l2)
 
-    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
-    l3 = tf.nn.relu(l3)
+    #l3 = tf.add(tf.matmul(l1, hidden_3_layer['weights']), hidden_3_layer['biases'])
+    #l3 = tf.nn.relu(l3)
 
-    output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
+    output = tf.matmul(l1, output_layer['weights']) + output_layer['biases']
 
     return output
 
@@ -72,15 +101,22 @@ def train_neural_network(x):
 
         for epoch in range(hm_epochs):
             epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples/batch_size)):
-                epoch_x,epoch_y = mnist.train.next_batch(batch_size)
-                _,c = sess.run([optimizer,cost], feed_dict={x:epoch_x,y:epoch_y})
+            i = 0
+            while i < len(train_x):
+                start = i
+                end = i + batch_size
+
+                batch_x = np.array(train_x[start:end])
+                #x = tf.reshape(x, [len(batch_x),len(train_x)])
+                batch_y = np.array(train_y[start:end])
+                _,c = sess.run([optimizer,cost], feed_dict={x:batch_x,y:batch_y})
                 epoch_loss += c
-            print('Epoch', epoch, "completed out of", hm_epochs, 'loss:',epoch_loss)
+                i += batch_size
+            print('Epoch', epoch+1, "completed out of", hm_epochs, 'loss:',epoch_loss)
 
         correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
         accuracy = tf.reduce_mean(tf.cast(correct,'float'))
-        print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+        print('Accuracy:',accuracy.eval({x:test_x, y:test_y}))
 
 train_neural_network(x)
 
